cebf3c14a15e03274e4a7491829194afd0ae1ff4
psburl <wesley.burlani@hub2b.com.br>
2018-09-05 23:03:51 -0300

    .

---
+++ RoutingAndSpectrumAllocation/RSA/DemandLinkPair.cs
@@ -0,0 +1,22 @@
+﻿using RoutingAndSpectrumAllocation.Demands;
+using RoutingAndSpectrumAllocation.Graphs;
+
+namespace RoutingAndSpectrumAllocation.RSA
+{
+    public class DemandLinkPair
+    {
+        public DemandLinkPair(Demand demand, GraphPath path)
+        {
+            Demand = demand;
+            Path = path;
+        }
+
+        public DemandLinkPair()
+        {
+
+        }
+
+        public Demand Demand { get; set; }
+        public GraphPath Path { get; set; }
+    }
+}
