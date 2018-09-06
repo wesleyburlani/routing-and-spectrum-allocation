dd9a323eaac5f8998714f86a35a73878435eb7c7
psburl <wesley.burlani@hub2b.com.br>
2018-09-05 22:50:37 -0300

    improve code pattern

--- RoutingAndSpectrumAllocation/RoutingAndSpectrumAllocation.cs
+++ RoutingAndSpectrumAllocation/RoutingAndSpectrumAllocation.cs
@@ -9,26 +9,23 @@ using System.Threading.Tasks;
 
 namespace RoutingAndSpectrumAllocation
 {
-    public class RoutingAndSpectrumAllocation : IRoutingAndSpectrumAllocation
+    public abstract class RoutingAndSpectrumAllocation : IRoutingAndSpectrumAllocation
     {
-        IGraphInputReader InputReader { get; set; }
-        IProgramLogger InfoLogger { get; set; }
-        ILogger StorageLogger { get; set; }
-        IPathSearcher PathSearcher { get; set; }
-        IRSATableFill RSATableFill { get; set; }
-        int supplied = 0;
+        protected IGraphInputReader InputReader { get; set; }
+        protected IProgramLogger InfoLogger { get; set; }
+        protected ILogger StorageLogger { get; set; }
+        protected IRSATableFill RSATableFill { get; set; }
+        protected int supplied = 0;
 
         public RoutingAndSpectrumAllocation(
             IGraphInputReader inputReader, 
             IProgramLogger infologger, 
             ILogger storageLogger, 
-            IPathSearcher pathSearcher,
             IRSATableFill RSATableFill)
         {
             InputReader = inputReader;
             InfoLogger = infologger;
             StorageLogger = storageLogger;
-            PathSearcher = pathSearcher;
             this.RSATableFill = RSATableFill;
         }
 
@@ -49,60 +46,9 @@ namespace RoutingAndSpectrumAllocation
             await ApplyRSA(graph, demands, numberOfLinkChannels);
         }
 
-        private async Task ApplyRSA(Graph graph, List<Demand> demands, int numberOfLinkChannels)
-        {
-            RSATable table = new RSATable(graph.Links, numberOfLinkChannels);
+        protected abstract Task ApplyRSA(Graph graph, List<Demand> demands, int numberOfLinkChannels);
 
-            foreach (Demand demand in demands)
-            {
-                GraphNode nodeFrom = graph.Nodes.FirstOrDefault(r => r.Id == demand.NodeIdFrom);
-                GraphNode nodeTo = graph.Nodes.FirstOrDefault(r => r.Id == demand.NodeIdTo);
-
-                await InfoLogger.LogInformation($"processing demand from {demand.NodeIdFrom} to {demand.NodeIdTo} with {demand.Slots} slots\n");
-
-                List<GraphPath> paths = PathSearcher.GetPaths(graph, nodeFrom, nodeTo, 2);
-
-                if (paths.Count() == 0)
-                {
-                    await InfoLogger.LogInformation($"path from {demand.NodeIdFrom} to {demand.NodeIdTo} not found.\n");
-                    continue;
-                }
-
-                table = await FillTable(table, graph,  demand, paths);
-            }
-
-            await InfoLogger.LogInformation($"Finished\n");
-
-            await InfoLogger.LogInformation($"Total Demands: {demands.Count}\nSupplied: {supplied}\nBlocked: {demands.Count - supplied}\n");
-
-            await InfoLogger.LogInformation(table.ToStringTable());
-        }
-
-        private async Task<RSATable> FillTable(RSATable table, Graph graph, Demand demand, List<GraphPath> paths)
-        {
-            bool filled = false;
-            foreach (var path in paths)
-            {
-                await InfoLogger.LogInformation($"trying path: {string.Join("->", path.Path)}");
-
-                List<AvailableSlot> availableTableSlots = GetAvailableTableSlots(graph, path, table);
-
-                if (RSATableFill.FillDemandOnTable(ref table, graph, demand, path, availableTableSlots))
-                {
-                    filled = true;
-                    await InfoLogger.LogInformation($"demand supplied\n");
-                    await InfoLogger.LogInformation(table.ToStringTable());
-                    supplied++;
-                    break;
-                }
-            }
-
-            if (filled == false) 
-                await InfoLogger.LogInformation($"It's not possible to supply demand of {demand.Slots}  from {demand.NodeIdFrom} to {demand.NodeIdTo}\n");
-            return table;
-        }
-
-        private List<AvailableSlot> GetAvailableTableSlots(Graph graph, GraphPath path, RSATable table)
+        protected virtual List<AvailableSlot> GetAvailableTableSlots(Graph graph, GraphPath path, RSATable table)
         {
             List<AvailableSlot> availableSlots = new List<AvailableSlot>();
             List<GraphLink> pathLinks = path.ToLinks(graph.Links);
--- RoutingAndSpectrumAllocation/SingleRSA.cs
+++ RoutingAndSpectrumAllocation/SingleRSA.cs
@@ -3,27 +3,25 @@ using RoutingAndSpectrumAllocation.Graphs;
 using RoutingAndSpectrumAllocation.InputReaders;
 using RoutingAndSpectrumAllocation.Loggers;
 using RoutingAndSpectrumAllocation.RSA;
-using RoutingAndSpectrumAllocation.Utils;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace RoutingAndSpectrumAllocation
 {
-    public class DedicatedProtectionRSA : RoutingAndSpectrumAllocation
+    public class SingleRSA : RoutingAndSpectrumAllocation
     {
-        IDisjointedPathPairSearcher DisjointedPathPairSearcher { get; set; }
+        IPathSearcher PathSearcher { get; set; }
 
-        public DedicatedProtectionRSA(
+        public SingleRSA(
             IGraphInputReader inputReader, 
             IProgramLogger infologger, 
-            ILogger storageLogger,
-            IDisjointedPathPairSearcher disjointedPathPairSearcher,
-            IRSATableFill RSATableFill)
+            ILogger storageLogger, 
+            IPathSearcher pathSearcher,
+            IRSATableFill RSATableFill) 
             : base(inputReader, infologger, storageLogger, RSATableFill)
         {
-            DisjointedPathPairSearcher = disjointedPathPairSearcher;
+            PathSearcher = pathSearcher;
         }
 
         protected override async Task ApplyRSA(Graph graph, List<Demand> demands, int numberOfLinkChannels)
@@ -37,7 +35,7 @@ namespace RoutingAndSpectrumAllocation
 
                 await InfoLogger.LogInformation($"processing demand from {demand.NodeIdFrom} to {demand.NodeIdTo} with {demand.Slots} slots\n");
 
-                List<Tuple<GraphPath, GraphPath>> paths = DisjointedPathPairSearcher.GetDisjointedPaths(graph, nodeFrom, nodeTo, 1, 1);
+                List<GraphPath> paths = PathSearcher.GetPaths(graph, nodeFrom, nodeTo, 2);
 
                 if (paths.Count() == 0)
                 {
@@ -45,7 +43,7 @@ namespace RoutingAndSpectrumAllocation
                     continue;
                 }
 
-                table = await FillTable(table, graph, demand, paths);
+                table = await FillTable(table, graph,  demand, paths);
             }
 
             await InfoLogger.LogInformation($"Finished\n");
@@ -55,32 +53,22 @@ namespace RoutingAndSpectrumAllocation
             await InfoLogger.LogInformation(table.ToStringTable());
         }
 
-        private async Task<RSATable> FillTable(RSATable table, Graph graph, Demand demand, List<Tuple<GraphPath, GraphPath>> paths)
+        private async Task<RSATable> FillTable(RSATable table, Graph graph, Demand demand, List<GraphPath> paths)
         {
             bool filled = false;
             foreach (var path in paths)
             {
-                var tableMemory = table.CopyObject<RSATable>();
+                await InfoLogger.LogInformation($"trying path: {string.Join("->", path.Path)}");
 
-                await InfoLogger.LogInformation($"trying main path: {string.Join("->", path.Item1.Path)}");
+                List<AvailableSlot> availableTableSlots = GetAvailableTableSlots(graph, path, table);
 
-                List<AvailableSlot> availableTableSlots = GetAvailableTableSlots(graph, path.Item1, tableMemory);
-
-                if (RSATableFill.FillDemandOnTable(ref tableMemory, graph, demand, path.Item1, availableTableSlots))
+                if (RSATableFill.FillDemandOnTable(ref table, graph, demand, path, availableTableSlots))
                 {
-                   availableTableSlots = GetAvailableTableSlots(graph, path.Item2, tableMemory);
-
-                    await InfoLogger.LogInformation($"trying secundary path: {string.Join("->", path.Item2.Path)}");
-
-                    if (RSATableFill.FillDemandOnTable(ref tableMemory, graph, demand, path.Item2, availableTableSlots, true))
-                    {
-                        filled = true;
-                        await InfoLogger.LogInformation($"demand supplied\n");
-                        table = tableMemory;
-                        await InfoLogger.LogInformation(table.ToStringTable());
-                        supplied++;
-                        break;
-                    }
+                    filled = true;
+                    await InfoLogger.LogInformation($"demand supplied\n");
+                    await InfoLogger.LogInformation(table.ToStringTable());
+                    supplied++;
+                    break;
                 }
             }
 
